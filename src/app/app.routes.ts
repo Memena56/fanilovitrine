d13f61cf9005f2ee953e75d114edb4f17a6b5529
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-05-22 16:08:32 +0300

    added contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', loadChildren: () => import('./features/home/home.routes').then(m => m.homeRoutes) },
     { path: 'tantara', loadChildren: () => import('./features/about/about.routes').then(m => m.aboutRoutes) },
-    { path: 'contact', loadChildren: () => import('./features/contact/contact.routes').then(m => m.contactRoutes) },
+    { path: 'ivotoerana', loadChildren: () => import('./features/contact/contact.routes').then(m => m.contactRoutes) },
     { path: 'hetsika', loadChildren: () => import('./features/events/events.routes').then(m => m.eventsRoutes) },
-    { path: 'shop', loadChildren: () => import('./features/articles/articles.routes').then(m => m.articlesRoutes) },
+    { path: 'shop', loadChildren: () => import('./features/articles/articles.routes').then(m => m.articlesRoutes) }
 ];
