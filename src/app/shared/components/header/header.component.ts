fe94924d6e12392097d18d07d270db23a5e4f131
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-05-22 17:09:00 +0300

    add footer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,11 +13,39 @@ export class HeaderComponent {
   isAboutFlyoutOpen = false;
   isMobileAboutSubMenuOpen = false;
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.isMobileMenuOpen && !this.elementRef.nativeElement.contains(event.target as Node)) {
+      this.closeMobileMenu();
+    }
+    const aboutFlyoutButton = this.elementRef.nativeElement.querySelector('#aboutFlyoutButton');
+    const aboutFlyoutContent = this.elementRef.nativeElement.querySelector('#aboutFlyoutContent');
+
+    if (
+      this.isAboutFlyoutOpen &&
+      !aboutFlyoutButton?.contains(event.target as Node) &&
+      !aboutFlyoutContent?.contains(event.target as Node)
+    ) {
+      this.isAboutFlyoutOpen = false;
+    }
+
+    const mobileAboutSubMenuButton = this.elementRef.nativeElement.querySelector('#mobileAboutSubMenuButton');
+    const mobileAboutSubMenuContent = this.elementRef.nativeElement.querySelector('#mobileAboutSubMenuContent');
+
+    if (
+      this.isMobileAboutSubMenuOpen &&
+      !mobileAboutSubMenuButton?.contains(event.target as Node) &&
+      !mobileAboutSubMenuContent?.contains(event.target as Node)
+    ) {
+      this.isMobileAboutSubMenuOpen = false;
+    }
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
-    if (this.isMobileMenuOpen && this.isAboutFlyoutOpen) {
+    if (this.isMobileMenuOpen) {
       this.isAboutFlyoutOpen = false;
     }
   }
@@ -29,7 +57,7 @@ export class HeaderComponent {
 
   toggleAboutFlyout() {
     this.isAboutFlyoutOpen = !this.isAboutFlyoutOpen;
-    if (this.isAboutFlyoutOpen && this.isMobileMenuOpen) {
+    if (this.isAboutFlyoutOpen) {
       this.isMobileMenuOpen = false;
       this.isMobileAboutSubMenuOpen = false;
     }
