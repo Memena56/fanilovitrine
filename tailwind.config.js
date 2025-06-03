457260be1de843cab3b4fd6a862c6db499899adb
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-06-03 16:54:35 +0300

    Added design animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
+      color: {
         "primary": "#2A255E",
         "secondary": "#FFDA00",
+      },
+
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+        }
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
       }
     },
   },
