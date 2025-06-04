22c4d6b42d514f447b5e6733d4d10dec707a309c
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-06-04 11:58:04 +0300

    Still struggling with the tailwind problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      color: {
+      colors: {
         "primary": "#2A255E",
         "secondary": "#FFDA00",
-      },
-
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0px)' },
-          '50%': { transform: 'translateY(-10px)' },
-        }
-      },
-      animation: {
-        float: 'float 3s ease-in-out infinite',
       }
     },
   },
