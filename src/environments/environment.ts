d8e25ad138c2d3b0a6391a6f85553b868d3f306e
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-10-14 13:04:26 +0300

    updated environments.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: "https://fanilovitrineback.onrender.com",
-    // apiUrl:'http://localhost:3000'
+    production: true,
+    apiUrl: "https://fanilonimadagasikara.mg/api",
 };
