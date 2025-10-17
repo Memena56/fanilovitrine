a95968b59cfc84f393f043dc11ebf81aa8375e04
Memena56 <nomenjanahary.florencia56@gmail.com>
2025-10-17 15:24:57 +0300

    Upadtes sidebar about-dioceses

--- src/app/features/about/about-dioceses/about-dioceses.component.ts
+++ src/app/features/about/about-dioceses/about-dioceses.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutSidebarComponent } from '../about-sidebar/about-sidebar.component';
 
 @Component({
   selector: 'app-about-dioceses',
-  imports: [],
+  imports: [CommonModule, AboutSidebarComponent],
   templateUrl: './about-dioceses.component.html',
   styleUrl: './about-dioceses.component.css'
 })
-export class AboutDiocesesComponent {
+export class AboutDiocesesComponent implements OnInit {
+  animationClass = 'page-enter';
 
+  ngOnInit(): void {
+      setTimeout(() => {
+        this.animationClass = 'page-enter page-enter-active';
+      }, 50);
+  }
 }
